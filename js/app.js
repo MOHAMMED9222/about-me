80e19534ac0de3c8fb4ad4336794ee44fc481427
Deon Curry <deoncvn68@gmail.comm>
2023-01-26 13:16:22 -0800

    Q1

--- js/app.js
+++ js/app.js
@@ -12,38 +12,42 @@ console.log('Greetings ' + username)
 // ! the logical not 
 // ${username} money curly
 
-let doilikewrestling = prompt("Do i like wrestling? YES or NO?").toLowerCase();
-if (doilikewrestling === 'yes' || doilikewrestling === 'y'){
-  console.log('you are correct! i love wrestling')
+function Questio1() {
+  let doilikewrestling = prompt("Do i like wrestling? YES or NO?").toLowerCase();
+  if (doilikewrestling === 'yes' || doilikewrestling === 'y') {
+    console.log('you are correct! i love wrestling')
+  }
 }
+Questio1();
+
 let whoisbetter = prompt("Who is better? CENA or ORTON?").toLowerCase();
-if (whoisbetter === 'cena' || whoisbetter === 'c'){
+if (whoisbetter === 'cena' || whoisbetter === 'c') {
   console.log('Thats right, YOU CANT SEE MEEEE!');
 } else if (whoisbetter === 'orton') {
   // console.log('Not even his dad was better');
   alert('Not even his dad was better');
 }
 let whoownswwe = prompt('Who owns the WWE? Vince or Triple H?').toLowerCase();
-if (whoownswwe === 'vince' || whoownswwe === 'v'){
+if (whoownswwe === 'vince' || whoownswwe === 'v') {
   // console.log('Yes, he does');
   alert('Yes, he does');
-} else if (whoisbetter === 'triple h' || 'hhh'){
+} else if (whoisbetter === 'triple h' || 'hhh') {
   // console.log('That is incorrect');
   alert('That is incorrect');
 }
 let whatisbetter = prompt('STARCADE OR WRESTLEMANIA?').toLowerCase();
-if (whatisbetter === 'starcade'){
+if (whatisbetter === 'starcade') {
   // console.log('Old school but i prefer Wrestlemania');
   alert('Old school but i prefer Wrestlemania');
-} else if (whatisbetter === 'wrestlemania'){
+} else if (whatisbetter === 'wrestlemania') {
   // console.log('of course, so many classics');
   alert('of course, so many classics');
 }
 let cantyoutell = prompt('Cant you tell i love wrestling?YES OR NO?').toLowerCase();
-if (cantyoutell === 'yes' || 'y'){
+if (cantyoutell === 'yes' || 'y') {
   // console.log('LETS GO');
   alert('LETS GO');
-} else if (cantyoutell === 'no' || 'n'){
+} else if (cantyoutell === 'no' || 'n') {
   // console.log('OFF THE TOP ROPE!!!');
   alert('OFF THE TOP ROPE!!!');
 }
@@ -64,47 +68,47 @@ while (attemptsThatRemain > 0) {
     console.log(myFavChampionship[i]);
     if (myFavChampionship[i].toLowerCase() === userResponse.toLowerCase()) {
       console.log('You are correct!')
-      score++; 
+      score++;
       answeredCorrectly = true
-      attemptsThatRemain = 0 
-    } 
-  } 
-  if (!answeredCorrectly){
-  userResponse = prompt('What is my favorite wwe title?') 
+      attemptsThatRemain = 0
+    }
+  }
+  if (!answeredCorrectly) {
+    userResponse = prompt('What is my favorite wwe title?')
   }
 }
 
 let attemptsRemaining = 4;
 let howmanyLosses = 2
 
- while (attemptsRemaining) {
-   attemptsRemaining--;
- // code that runs as long as the condition is 
- // evaluated to true
-let userResponse = prompt('How many times did Taker lose at Wrestlemania?')
-   userResponse = Number(userResponse)
+while (attemptsRemaining) {
+  attemptsRemaining--;
+  // code that runs as long as the condition is 
+  // evaluated to true
+  let userResponse = prompt('How many times did Taker lose at Wrestlemania?')
+  userResponse = Number(userResponse)
 
-   if(userResponse === howmanyLosses){
-     console.log('that is correct');
-     break 
-   } else {
-     console.log('that is wrong');
-     if (userResponse > howmanyLosses) {
-       console.log('too high, try lower!');
-     } else {
-       console.log('thats toooo low');
-       }
-     }  
-   console.log(`the users has ${attemptsRemaining} attempts remaining`);
+  if (userResponse === howmanyLosses) {
+    console.log('that is correct');
+    break
+  } else {
+    console.log('that is wrong');
+    if (userResponse > howmanyLosses) {
+      console.log('too high, try lower!');
+    } else {
+      console.log('thats toooo low');
+    }
+  }
+  console.log(`the users has ${attemptsRemaining} attempts remaining`);
 
-   if (attemptsRemaining === 0) {
-     console.log('the answer is 2');
-   }
-   }
+  if (attemptsRemaining === 0) {
+    console.log('the answer is 2');
+  }
+}
 // console.log('Enjoy the website ' + username)
 
 
 // let x = 4;
 // if (x < 5) {
-  // this code will run if x < 5 
+  // this code will run if x < 5
 //}
